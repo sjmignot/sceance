511090b2693a128995c2619db2e193788d56c8d9
sjmignot <sjmignot@gmail.com>
2019-12-30 17:55:39 +0100

    cleaned up get_movie_watchlist_overlap code

--- get_showtime_watchlist.py
+++ get_showtime_watchlist.py
@@ -1,18 +1,21 @@
-"""Starts a Firfox headless brower to see if movies on your watchlist are playing
+'''
+Starts a Firfox headless brower to see if movies on your watchlist are playing
 at any of your favorite theaters.
 
 Favorite theaters are taken from a txt file (extracted from "theaters.txt").
 These showtimes are compared to a watchlist (extracted from "watchlist.txt")
 
 -samuel mignot-
-"""
+'''
+
 import datetime
 import re
 import collections
 import selenium
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+import os
 
 WATCHLIST_FILE = "watchlist.txt"
 THEATERS_FILE = "theaters.txt"
@@ -24,10 +27,9 @@ GOOGLE_CSS_SELECTORS = {
 }
 
 Theater = collections.namedtuple('Theater', ['name', 'address'])
-theater_search = "https://www.google.com/search?q={theater_name}+showtimes"
 
 def get_dates(datelist):
-    """Do X and return a list."""
+    '''Takes a string of concatenated standard dates and return a list of military time tuples.'''
     date_pattern = r"\d{1,2}\:\d{2}(?:am|pm)"
     matches = re.findall(date_pattern, datelist)
     tupled_matches = [list(map(int, match[:-2].split(":")))+[match[-2:]] for match in matches]
@@ -37,21 +39,22 @@ def get_dates(datelist):
     return military_time_tuples
 
 def get_watchlist():
-    """Do X and return a list."""
+    '''Reads the watchlist file and returns a set of movies with titles lowercased.'''
     with open(WATCHLIST_FILE) as f:
-        return set((x[:-1]).lower() for x in f.readlines())
+        return frozenset((x[:-1]).lower() for x in f.readlines())
 
 def get_theaters():
-    """Do X and return a list."""
+    """Reads the theaters file and returns a set of theaters."""
     with open(THEATERS_FILE) as f:
         return f.readlines()
 
-def main():
-    """Do X and return a list."""
+def main(headless=True):
+    '''Do X and return a list.'''
     movie_showtimes = {}
+    theater_search = "https://www.google.com/search?q={theater_name}+showtimes"
 
     options = Options()
-    options.headless = True
+    options.headless = headless
 
     profile = webdriver.FirefoxProfile()
     profile.set_preference('intl.accept_languages', 'en_GB')
@@ -59,28 +62,29 @@ def main():
     driver = webdriver.Firefox(
         executable_path=GeckoDriverManager().install(),
         firefox_profile=profile,
-        options=options
+        options=options,
+        service_log_path=os.path.devnull
     )
 
     watchlist = get_watchlist()
     theaters = get_theaters()
     for theater in theaters:
         url = theater_search.format(theater_name=(theater[:-1].replace(" ", "+")))
-        print(url)
         driver.get(url)
         address = driver.find_element_by_css_selector(GOOGLE_CSS_SELECTORS['address']).text
         movie_theater = Theater(name=theater[:-1], address=address)
-        print(movie_theater)
-        print("")
+        print(f"{movie_theater}\n")
         show_days = driver.find_elements_by_css_selector(GOOGLE_CSS_SELECTORS['show_days'])
         date_s = datetime.datetime.today()
         for i, day in enumerate(show_days):
             try:
-                see_more_button = driver.find_element_by_css_selector(GOOGLE_CSS_SELECTORS['see_more'])
-                see_more_button.click()
+                see_more_button = driver.find_element_by_css_selector(
+                    GOOGLE_CSS_SELECTORS['see_more']
+                )
+                driver.execute_script("arguments[0].click();", see_more_button)
+                # see_more_button.click()
             except selenium.common.exceptions.NoSuchElementException:
                 pass
-            date_s += datetime.timedelta(days=1)
             day.click()
             showings = driver.find_elements_by_css_selector(GOOGLE_CSS_SELECTORS['showings'])
             for showing in showings:
@@ -94,6 +98,7 @@ def main():
                         movie_showtimes[name].append((movie_theater, possible_showtime))
                     else:
                         movie_showtimes[name] = [(movie_theater, possible_showtime)]
+            date_s += datetime.timedelta(days=1)
     driver.quit()
     print(movie_showtimes)
     for key, value in movie_showtimes.items():
