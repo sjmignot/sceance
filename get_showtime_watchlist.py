5c8adf529a6d84d6c6fb6dd6c5e8056afcded625
sjmignot <sjmignot@gmail.com>
2019-12-31 11:01:50 +0100

    cleaned up showtime_watchlist file

--- get_showtime_watchlist.py
+++ get_showtime_watchlist.py
@@ -12,10 +12,15 @@ import os
 import datetime
 import re
 import collections
+import time
+import pickle
 import selenium
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
+from contextlib import contextmanager
 
 WATCHLIST_FILE = "watchlist.txt"
 THEATERS_FILE = "theaters.txt"
@@ -23,10 +28,13 @@ GOOGLE_CSS_SELECTORS = {
     "see_more": "div.hide-focus-ring.iXqz2e.aI3msd.xpdarr.pSO8Ic.vk_arc",
     "address": "span.LrzXr",
     "show_days": "li.tb_l",
-    "showings": "div.lr_c_fcb.lr-s-stor"
+    "showings": "div.lr_c_fcb.lr-s-stor",
+    "film_length": "div.wwUB2c.PZPZlf"
 }
+FILM_DETAIL_SEP = '‧'
 
 Theater = collections.namedtuple('Theater', ['name', 'address'])
+Film = collections.namedtuple('Film', ['name','release', 'genre', 'length'])
 
 def get_dates(datelist):
     '''Takes a string of concatenated standard dates and return a list of military time tuples.'''
@@ -35,7 +43,6 @@ def get_dates(datelist):
     tupled_matches = [list(map(int, match[:-2].split(":")))+[match[-2:]] for match in matches]
     military_time_tuples = [tuple([match[0]+12, match[1]]) if match[2] == 'pm'
                             else tuple(match[:2]) for match in tupled_matches]
-
     return military_time_tuples
 
 def get_watchlist():
@@ -44,15 +51,48 @@ def get_watchlist():
         return frozenset((x[:-1]).lower() for x in f.readlines())
 
 def get_theaters():
-    """Reads the theaters file and returns a set of theaters."""
+    '''Reads the theaters file and returns a set of theaters.'''
     with open(THEATERS_FILE) as f:
         return f.readlines()
 
-def main(headless=True, auto_filter_work=True):
-    '''Do X and return a list.'''
-    movie_showtimes = {}
-    theater_search = "https://www.google.com/search?q={theater_name}+showtimes"
+@contextmanager
+def wait_for_new_window(driver, timeout=10):
+    handles_before = driver.window_handles
+    yield
+    WebDriverWait(driver, timeout).until(
+        lambda driver: len(handles_before) != len(driver.window_handles))
 
+
+def get_movie_lengths(film_links):
+    '''Takes a dictionary of film links and gets movie lengths if they haven't already been saved.'''
+    film_length_dict = {}
+    if os.path.exists('film_length.pickle'):
+        with open('film_length.pickle', 'rb') as flf:
+            film_length_dict = pickle.load(flf)
+
+    new_films = { k: v for k, v in film_links.items() if k not in film_length_dict.keys()}
+    if(not new_films): return film_length_dict
+
+    for k, url in new_films.items():
+        driver.get(url)
+        film_details = driver.find_element_by_css_selector(GOOGLE_CSS_SELECTORS['film_length']).text
+        release_date, film_genre, film_length = film_details.split(FILM_DETAIL_SEP)
+        cur_film  = Film(
+            name = k.strip(),
+            release = release_date.strip(),
+            genre = film_genre.strip(),
+            length = tuple(film_length[:-1].strip().split('h '))
+        )
+        print(cur_film)
+        film_length_dict[k] = cur_film
+
+    with open('film_length.pickle', 'wb') as flf:
+        pickle.dump(film_length_dict, flf)
+
+    return film_length_dict
+
+def start_brower(headless):
+    '''starts and returns a selenium firefox brower. Takes a paremeter to determine headedness'''
     options = Options()
     options.headless = headless
 
@@ -65,11 +105,22 @@ def main(headless=True, auto_filter_work=True):
         options=options,
         service_log_path=os.path.devnull
     )
+    return driver
+
+def main(headless=True, auto_filter_work=True):
+    '''Do X and return a list.'''
+    theater_search_placeholder = "https://www.google.com/search?q={theater_name}+showtimes"
+
+    movie_showtimes = {}
+    film_links = {}
+
+    driver = start_brower(headless)
 
     watchlist = get_watchlist()
     theaters = get_theaters()
+
     for theater in theaters:
-        url = theater_search.format(theater_name=(theater[:-1].replace(" ", "+")))
+        url = theater_search_placeholder.format(theater_name=(theater[:-1].replace(" ", "+")))
         driver.get(url)
         address = driver.find_element_by_css_selector(GOOGLE_CSS_SELECTORS['address']).text
         movie_theater = Theater(name=theater[:-1], address=address)
@@ -77,20 +128,23 @@ def main(headless=True, auto_filter_work=True):
         show_days = driver.find_elements_by_css_selector(GOOGLE_CSS_SELECTORS['show_days'])
         date_s = datetime.datetime.today()
         for i, day in enumerate(show_days):
+            day.click()
             try:
                 see_more_button = driver.find_element_by_css_selector(
                     GOOGLE_CSS_SELECTORS['see_more']
                 )
                 driver.execute_script("arguments[0].click();", see_more_button)
-                # see_more_button.click()
             except selenium.common.exceptions.NoSuchElementException:
                 pass
-            day.click()
             showings = driver.find_elements_by_css_selector(GOOGLE_CSS_SELECTORS['showings'])
             for showing in showings:
                 if not len(showing.text.splitlines()) == 3:
                     continue
+
                 name, datelist = showing.text.splitlines()[1:]
+                a = showing.find_element_by_css_selector('a')
+                film_links[name] = a.get_attribute('href')
+
                 show_times = get_dates(datelist)
                 for show_time in show_times:
                     possible_showtime = date_s.replace(hour=show_time[0], minute=show_time[1])
@@ -100,6 +154,7 @@ def main(headless=True, auto_filter_work=True):
                         movie_showtimes[name] = [(movie_theater, possible_showtime)]
             date_s += datetime.timedelta(days=1)
     driver.quit()
+
     print(movie_showtimes)
     possible_showtimes = {}
     for key, value in movie_showtimes.items():
@@ -108,6 +163,9 @@ def main(headless=True, auto_filter_work=True):
             print(f"You can watch {key} at:")
             for i in value:
                 print(f"- {i[0].name} at {i[1]}")
+    film_links = { k: v for k, v in film_links.items() if k.lower() in watchlist }
+    film_length_dict = get_movie_lengths(film_links)
+    print(film_length_dict)
 
 if __name__ == "__main__":
     main()
--- showtimes_to_google_cal.py
+++ showtimes_to_google_cal.py
@@ -18,21 +18,43 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-cal_id = os.environ['GOOGLE_CAL_ID']
-cal_api_key = os.environ['GOOGLE_CAL_API_KEY']
+def build_event(service):
+    event = {
+      'summary': 'Google I/O 2015',
+      'location': '800 Howard St., San Francisco, CA 94103',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': '2015-05-28T09:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'end': {
+        'dateTime': '2015-05-28T17:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+      },
+      'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ],
+      'attendees': [
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'},
+      ],
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+    event = service.events().insert(calendarId='primary', body=event).execute()
 
 
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+def get_creds():
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -41,25 +63,18 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
+
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = get_creds()
     service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    build_event(service)
 
 if __name__ == '__main__':
     main()
