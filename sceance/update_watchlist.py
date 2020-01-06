7c9dfce120f688e49670251df7450e94b06afc38
sjmignot <sjmignot@gmail.com>
2020-01-06 14:26:41 +0100

    complete update watchlist script

--- sceance/update_watchlist.py
+++ sceance/update_watchlist.py
@@ -1,5 +1,11 @@
-import requests
+'''
+interact with your update_watchlist by downloading watchlist from letterboxd.
+'''
+
 import os
+import requests
+import zipfile
+import pandas as pd
 
 SIGN_IN_PAGE = "https://letterboxd.com/user/login.do"
 EXPORT_PAGE = "https://letterboxd.com/data/export"
@@ -8,25 +14,45 @@ REFERER = "https://letterboxd.com/activity/"
 SIGN_IN_ID_EL = "username"
 SIGN_IN_PW_EL = "password"
 COOKIE_NAME = "com.xk72.webparts.csrf"
+ZIP_FILE = 'data/lb_data.zip'
+WATCHLIST_CSV = 'watchlist.csv'
+DATA = 'data/'
+WATCHLIST_TXT = 'watchlist.txt'
 
 payload = {
     SIGN_IN_ID_EL: os.environ['LBXD_USERNAME'],
     SIGN_IN_PW_EL: os.environ['LBXD_PASSWORD']
 }
 
+
+def extract_watchlist():
+    with zipfile.ZipFile(ZIP_FILE, 'r') as zip_ref:
+        zip_ref.extract(WATCHLIST_CSV, path=DATA)
+    os.remove(ZIP_FILE)
+    watchlist = list(pd.read_csv(f'data/{WATCHLIST_CSV}')['Name'])
+    with open(f"{DATA}{WATCHLIST_TXT}", 'w') as f:
+        f.write("\n".join(watchlist))
+    os.remove(f"{DATA}{WATCHLIST_CSV}")
+
+def download_watchlist():
+    '''update watchlist by '''
+    with requests.Session() as session:
+        session.get(SIGN_IN_PAGE)
+        if COOKIE_NAME in session.cookies:
+            payload['__csrf'] = session.cookies[COOKIE_NAME]
+
+        # sign in
+        session.post(SIGN_IN_PAGE, data=payload, headers={'referer': REFERER})
+        letterboxd_content = session.get(EXPORT_PAGE)
+
+        with open(ZIP_FILE, 'wb') as f:
+            f.write(letterboxd_content.content)
+
 def update_watchlist():
-    with requests.Session() as s:
-        s.get(SIGN_IN_PAGE)
-        if COOKIE_NAME in s.cookies:
-            payload['__csrf'] = s.cookies[COOKIE_NAME]
+    download_watchlist()
+    extract_watchlist()
 
-        p = s.post(SIGN_IN_PAGE, data=payload, headers={'referer': REFERER})
-        r = s.get(EXPORT_PAGE)
-
-        print(r.content)
-
-        with open('data/lbdata.zip', 'wb') as f:
-            f.write(r.content)
+    pass
 
 if __name__ == "__main__":
     update_watchlist()
