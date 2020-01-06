fa4836a42941901a21b4cb8f4fcc4dead1f009ba
sjmignot <sjmignot@gmail.com>
2020-01-06 11:44:01 +0100

    started working on an update watchlist function

--- sceance/update_watchlist.py
+++ sceance/update_watchlist.py
@@ -1,5 +1,32 @@
+import requests
+import os
+
+SIGN_IN_PAGE = "https://letterboxd.com/user/login.do"
+EXPORT_PAGE = "https://letterboxd.com/data/export"
+REFERER = "https://letterboxd.com/activity/"
+
+SIGN_IN_ID_EL = "username"
+SIGN_IN_PW_EL = "password"
+COOKIE_NAME = "com.xk72.webparts.csrf"
+
+payload = {
+    SIGN_IN_ID_EL: os.environ['LBXD_USERNAME'],
+    SIGN_IN_PW_EL: os.environ['LBXD_PASSWORD']
+}
+
 def update_watchlist():
-    pass
-   
+    with requests.Session() as s:
+        s.get(SIGN_IN_PAGE)
+        if COOKIE_NAME in s.cookies:
+            payload['__csrf'] = s.cookies[COOKIE_NAME]
+
+        p = s.post(SIGN_IN_PAGE, data=payload, headers={'referer': REFERER})
+        r = s.get(EXPORT_PAGE)
+
+        print(r.content)
+
+        with open('data/lbdata.zip', 'wb') as f:
+            f.write(r.content)
+
 if __name__ == "__main__":
     update_watchlist()
