de5ea60e3e157d6e46e6562db8c375855f6194f7
sjmignot <sjmignot@gmail.com>
2020-01-06 14:46:31 +0100

    update watchlist cleaned

--- sceance/update_watchlist.py
+++ sceance/update_watchlist.py
@@ -2,14 +2,22 @@
 interact with your update_watchlist by downloading watchlist from letterboxd.
 '''
 
+#IMPORTS
+
 import os
-import requests
 import zipfile
+import requests
 import pandas as pd
+
+# CONTSTANTS
+
+# letterboxd urls
 
 SIGN_IN_PAGE = "https://letterboxd.com/user/login.do"
 EXPORT_PAGE = "https://letterboxd.com/data/export"
 REFERER = "https://letterboxd.com/activity/"
+
+# other  constants
 
 SIGN_IN_ID_EL = "username"
 SIGN_IN_PW_EL = "password"
@@ -19,13 +27,8 @@ WATCHLIST_CSV = 'watchlist.csv'
 DATA = 'data/'
 WATCHLIST_TXT = 'watchlist.txt'
 
-payload = {
-    SIGN_IN_ID_EL: os.environ['LBXD_USERNAME'],
-    SIGN_IN_PW_EL: os.environ['LBXD_PASSWORD']
-}
-
-
 def extract_watchlist():
+    '''extracts the watchlist from  the downloaded letterboxd content'''
     with zipfile.ZipFile(ZIP_FILE, 'r') as zip_ref:
         zip_ref.extract(WATCHLIST_CSV, path=DATA)
     os.remove(ZIP_FILE)
@@ -34,25 +37,29 @@ def extract_watchlist():
         f.write("\n".join(watchlist))
     os.remove(f"{DATA}{WATCHLIST_CSV}")
 
-def download_watchlist():
-    '''update watchlist by '''
+def download_letterboxd_content():
+    '''signs you into letterboxd and then downloads the content'''
+    login_payload = {
+        SIGN_IN_ID_EL: os.environ['LBXD_USERNAME'],
+        SIGN_IN_PW_EL: os.environ['LBXD_PASSWORD']
+    }
+
     with requests.Session() as session:
         session.get(SIGN_IN_PAGE)
         if COOKIE_NAME in session.cookies:
-            payload['__csrf'] = session.cookies[COOKIE_NAME]
+            login_payload['__csrf'] = session.cookies[COOKIE_NAME]
 
         # sign in
-        session.post(SIGN_IN_PAGE, data=payload, headers={'referer': REFERER})
+        session.post(SIGN_IN_PAGE, data=login_payload, headers={'referer': REFERER})
         letterboxd_content = session.get(EXPORT_PAGE)
 
         with open(ZIP_FILE, 'wb') as f:
             f.write(letterboxd_content.content)
 
 def update_watchlist():
-    download_watchlist()
+    '''updates your watchlist by downloading letterboxd content and then extracting it'''
+    download_letterboxd_content()
     extract_watchlist()
-
-    pass
 
 if __name__ == "__main__":
     update_watchlist()
