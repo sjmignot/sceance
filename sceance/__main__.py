9be003d193f6cee7fdf7bcbe7933894914a8843f
sjmignot <sjmignot@gmail.com>
2020-02-03 22:40:07 +0100

    reorder optional argparams

--- sceance/__main__.py
+++ sceance/__main__.py
@@ -53,14 +53,14 @@ parser = argparse.ArgumentParser(description='Discover which movies in your watc
 parser.add_argument('-b', '--browser', default=settings['browser'], choices=['firefox', 'chrome'],
         help=f"Whether you want to run a chrome (c) or firefox (f) headless browser (default: {settings['browser']})")
 
-parser.add_argument('-t', '--timezone', type=valid_timezone, default=settings['timezone'],
-        help=f"Make sure you provide a valid IANA timezone. (default: {settings['timezone']}).")
-
 parser.add_argument('-d', '--workdays', type=valid_workdays, default=settings['workdays'],
         help=f"Which days you work. A comma seperated list of numbers between 0 and 6 (0 is monday, 1 is tueday, ..., 6 is sunday). (default: {settings['workdays']}).")
 
 parser.add_argument('-w', '--workhours', type=valid_workhours, default=settings['workhours'],
                     help=f"Which hours do you work. Hours are in 24 hour time and formatted: start_hour,end_hour. (default: {settings['workhours']}).")
+
+parser.add_argument('-t', '--timezone', type=valid_timezone, default=settings['timezone'],
+        help=f"Make sure you provide a valid IANA timezone. (default: {settings['timezone']}).")
 
 args = vars(parser.parse_args())
 
