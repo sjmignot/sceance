82d5e96b65197ff96522324d6527fca6f18cc76b
sjmignot <sjmignot@gmail.com>
2020-02-26 13:56:29 +0100

    update version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name='sceance',
     author='Samuel Mignot',
     author_email='sjmignot@gmail.com',
-    version='1.0.2',
+    version='1.0.3',
     description='Uses a watchlist and favorite theaters to suggest film screenings.',
     long_description=long_description,
     long_description_content_type='text/markdown',
