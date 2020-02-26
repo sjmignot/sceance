356542bb65f8445d12670adc0612704597b5e56d
sjmignot <sjmignot@gmail.com>
2020-02-26 13:37:06 +0100

    update intro and sigint

--- sceance/film_to_cal.py
+++ sceance/film_to_cal.py
@@ -23,8 +23,32 @@ import filter_select_showings
 # constants           #
 # ------------------- #
 
+COLOR = {
+    'purple': '\033[95m',
+    'cyan': '\033[96m',
+    'darkcyan': '\033[36m',
+    'blue': '\033[94m',
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'red': '\033[91m',
+    'bold': '\033[1m',
+    'underline': '\033[4m',
+    'end': '\033[0m',
+}
+
+def bold(word):
+    return COLOR['bold']+word+COLOR['end']
+
+def underline(word):
+    return COLOR['underline']+word+COLOR['end']
+
 def film_to_cal(args, headless: bool = True):
     '''main file for getting showings and savaing them to a google calendar'''
+    print("")
+    print(f"running sceance with:")
+    print(f"{bold('watchlist')}: {args['watchlist']}")
+    print(f"{bold('theaters')}: {args['theaters']}")
+
     showings = get_showings.get_watchlist_showings(args['browser'], args['all_films'], args['theaters'], args['watchlist'], headless)
     projection_list = filter_select_showings.filter_select_showings(showings, args['workdays'], args['workhours'])
     if(len(projection_list)>0):
